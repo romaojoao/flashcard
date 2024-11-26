448c04812bc2f292e38aa90439fac1079a45d159
romaojoao <0000108033628xsp@al.educacao.sp.gov.br>
2024-11-26 09:11:46 -0300

    Add files via upload
    adicionando arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Corinthians',
+    'Quem é o maior artilheiro da história do Corinthians?',
+    'O maior artilheiro da história do clube é Cláudio com 306 gols'
+)
+
+criaCartao(
+    'Basquete',
+    'Qual é o maior jogador da franquia San Antonio Spurs da NBA?',
+    'O maior jogador da franquia é Tim Duncan'
+)
+
+criaCartao(
+    'One Piece',
+    'Qual a primeira recompensa de Usopp(Sogeking) na obra?',
+    'A primeira recompensa do personagem na obra é de 30 milhões de berries'
+)
+
+criaCartao(
+    'Batalha de rima',
+    'Qual o único MC da história que venceu 2 nacionais?',
+    'O primeiro e único MC da história a vencer 2 nacionais é Douglas Din'
+)
